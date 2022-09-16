a1f901fb360583b3b30f1559d206e62b9e8f7fc4
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2022-09-16 11:32:26 -0600

    Create checkWin() function
    This function checks for the pattern and gives a winner.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,52 +1,82 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import "./App.css";
 import Square from "./Components/Square";
 
 function App() {
-  const [board, setBoard] = useState(["","","","","","","","","",])
-  const [player, setPlayer] = useState('O');
+  const boardIntitialState = ["", "", "", "", "", "", "", "", ""];
 
-  useEffect(()=>{
+  const patterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
+  const [board, setBoard] = useState(boardIntitialState);
+  const [player, setPlayer] = useState("O");
+  const [isTie, setTie] = useState(false);
+  const [score, setScore] = useState({X : 0, Y : 0});
+
+  useEffect(() => {
+
+    checkWin()
+    
+    // Checking if it's a tie
+    !board.includes("") ? setTie(true) : setTie(false);
+    
+    if (isTie) resetGame();
+    
     // Changing player after turn
-    player === 'X' ? setPlayer('O') : setPlayer('X')
-    console.log('player changed')
-  }, [board])
- 
-   const move = (square) => {   
-     
-    // Change the square value
-    console.log(square)
+    player === "X" ? setPlayer("O") : setPlayer("X");
+  }, [board]);
 
+  const move = (square) => {
     setBoard(
-      board.map((val,indx)=>{
-        
-        if(square == indx && val == "")
-            return player
+      board.map((val, indx) => {
+        if (square == indx && val == "") return player;
 
         return val;
-      } )
-    )
+      })
+    );
+  };
 
+  const checkWin = () => {
+     patterns.forEach(pattern => {
+       const [s1, s2, s3] = pattern;
+       
+         if(board[s1] == '' || board[s2] == '' || board[s3] == '') return;
+         
+         if(board[s1] == board[s2] && board[s2] == board[s3] ){
+             
+         }
+     })
   }
+
+  const resetGame = () => {
+    setBoard(boardIntitialState);
+    setPlayer("O");
+  };
   return (
     <div className="app">
       <div className="board-wrapper">
         <div className="board-row">
-          <Square val={board[0]} handleClick={()=> move(0)} />
-          <Square val={board[1]} handleClick={()=> move(1)} />
-          <Square val={board[2]} handleClick={()=> move(2)} />
+          <Square val={board[0]} handleClick={() => move(0)} />
+          <Square val={board[1]} handleClick={() => move(1)} />
+          <Square val={board[2]} handleClick={() => move(2)} />
         </div>
         <div className="board-row">
-          <Square val={board[3]} handleClick={()=> move(3)} />
-          <Square val={board[4]} handleClick={()=> move(4)} />
-          <Square val={board[5]} handleClick={()=> move(5)} />
+          <Square val={board[3]} handleClick={() => move(3)} />
+          <Square val={board[4]} handleClick={() => move(4)} />
+          <Square val={board[5]} handleClick={() => move(5)} />
         </div>
         <div className="board-row">
-          <Square val={board[6]} handleClick={()=> move(6)} />
-          <Square val={board[7]} handleClick={()=> move(7)} />
-          <Square val={board[8]} handleClick={()=> move(8)} />
+          <Square val={board[6]} handleClick={() => move(6)} />
+          <Square val={board[7]} handleClick={() => move(7)} />
+          <Square val={board[8]} handleClick={() => move(8)} />
         </div>
       </div>
     </div>
